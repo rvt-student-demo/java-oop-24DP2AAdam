5649cfd44ea9fdee75553b854ead441ef524029d
24DP2AAdam <A240801AA@rvt.lv>
2026-05-18 11:28:39 +0300

    sqlite faila izveidosana

--- src/test/java/rvt/AppTest.java
+++ src/test/java/rvt/AppTest.java
@@ -1,5 +1,22 @@
 package rvt;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.sql.SQLException;
+
 public class AppTest {
-    // ...existing code...
+    public static void main(String[] args) {
+        try (
+            Connection connection = DriverManager.getConnection("jdbc:sqlite:todo.db");
+            Statement statement = connection.createStatement();  
+        )
+        {
+            String sql = "CREATE TABLE todo" + "(id INTEGER PRIMARY KEY, task TEXT NOT NULL, phone INTEGER) STRICT";
+            statement.executeUpdate(sql);  // Use the connection
+        } 
+        catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
